ac835f433a1bc4b0497cbac76d3c2e8be390a929
alexios voulimeneas <balexios@gmail.com>
2014-06-26 00:58:50 +0200

    small changes

---
+++ Lecture3_Assignment3/header.h
@@ -0,0 +1,10 @@
+#ifndef STDIO_H // include guard
+#define STDIO_H
+
+#include<stdio.h>
+
+extern int global; // the declaration merely introduces the variable into the symbol table (and will cause the linker to go looking for it when it comes to link time).
+// we should define global in only one source file and include o.h to all the source file that have global
+int func(void);
+
+#endif
