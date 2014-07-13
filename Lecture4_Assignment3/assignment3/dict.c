26d834c4ea6071b886933769c5b94537eef507bd
alexios voulimeneas <balexios@gmail.com>
2014-07-13 19:04:41 +0300

    Some more files added

---
+++ Lecture4_Assignment3/assignment3/dict.c
@@ -0,0 +1,15 @@
+#include "dict.h"
+/* data structure
+for the dictionary */
+//char * the_dictionary[1000];
+void load_dictionary(void)
+{
+    // ...
+    the_dictionary[1];
+}
+
+char * lookup(char dictionary[])
+{
+    // ...
+    return NULL;
+}
---
+++ Lecture4_Assignment3/assignment3/dict.h
@@ -0,0 +1,13 @@
+#ifndef DICT_H // include guard
+#define DICT_H
+
+#include<stdio.h>
+
+/* data structure
+for the dictionary */
+char* the_dictionary[1000];
+/* declarations */
+void load_dictionary();
+char* lookup(char []);
+
+#endif
