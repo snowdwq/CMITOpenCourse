bd4e3fbbbac1c6824163dfbeb1acb318435febe7
alexios voulimeneas <balexios@gmail.com>
2014-08-03 21:40:39 +0300

    Small change in test8.c

--- Lecture6/test8.c
+++ Lecture6/test8.c
@@ -3,15 +3,16 @@
 int main(void)
 {
     int l;
-    for(l = 0; l < 10; l++)
+    for(l = 0; l < 1000; l++)
     {
-        int* mpool = malloc(sizeof(int)*10000000);
+        int* mpool = malloc(sizeof(int)*100000);
         long i;
-        for(i = 0; i < 10000000; i++)
+        for(i = 0; i < 100000; i++)
         {
             int* k = &mpool[i];
             *k = 4;
         }
+        free(mpool);
     }
 
     return 0;
